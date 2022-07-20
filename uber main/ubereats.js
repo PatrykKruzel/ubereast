2af6c9b1f465a73f2d67ea8b5ac4034eecef8e5b
PatrykKruzel <108151718+PatrykKruzel@users.noreply.github.com>
2022-07-20 09:39:00 +0200

    Add files via upload

---
+++ uber main/ubereats.js
@@ -0,0 +1,60 @@
+function showmenu(){
+    document.getElementById("m").style.width = "14%";
+    document.getElementById("m").style.height = "100%";
+   document.getElementById("m").style.visibility= "visible";
+   document.getElementById("zaslona").style.visibility= "visible";
+   document.getElementById("zaslona").style.opacity= "0.5";
+}
+function hidemenu(){
+    document.getElementById("m").style.width = "0%";
+    document.getElementById("m").style.visibility= "hidden";
+    document.getElementById("zaslona").style.visibility= "hidden";
+    document.getElementById("root").style.opacity= "1";
+}
+
+function refresh(){
+    location.reload();
+}
+
+
+function podkresl(pomoc){
+    pomoc.style.textDecoration= "underline";
+}
+
+function niepodkresl(pomoc){
+    pomoc.style.textDecoration= "none";
+}
+
+
+
+function ciemnienie() {
+   document.getElementById("divsearch").style.boxShadow = "0px -2px 0px inset";
+}
+function ciemnienie1() {
+    document.getElementById("divsearch1").style.boxShadow = "0px -2px 0px inset";
+ }
+
+window.onscroll = function() {scroll()};
+
+ function scroll(){
+
+    if (document.documentElement.scrollTop > 50) {
+        document.getElementById("gora").style.backgroundColor = "white";
+    
+      } else {
+        document.getElementById("gora").style.backgroundColor = "";
+    
+      }
+
+      if (document.documentElement.scrollTop > 450) {
+        document.getElementById("divsearch1").style.width = "39%";
+         document.getElementById("divsearch1").style.visibility= "visible";
+      }
+      else {
+        document.getElementById("divsearch1").style.width = "0%";
+      document.getElementById("divsearch1").style.visibility = "hidden";
+      
+        }
+
+ }
+ 
