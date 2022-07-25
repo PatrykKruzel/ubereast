7638a168e4766002fb7f10894d77ae82acb90a52
PatrykKruzel <108151718+PatrykKruzel@users.noreply.github.com>
2022-07-25 14:00:40 +0200

    Add files via upload

--- uber main/ubereats.js
+++ uber main/ubereats.js
@@ -1,27 +1,29 @@
 function showmenu(){
   if(window.innerWidth<1500) {
     document.getElementById("m").style.width = "70%";
+    document.getElementById("m").style.opacity = "1";
     document.getElementById("m").style.visibility= "visible";
    document.getElementById("zaslona").style.visibility= "visible";
    document.getElementById("zaslona").style.opacity= "0.5";
-   document.getElementById("container").style.overflow = "hidden";
+   document.getElementById("container").style.overflowY = "hidden";
    document.getElementById("applink1").innerHTML = "Pobierz aplikację";
    }
 
    else {
-    document.getElementById("m").style.width = "14%";
+    document.getElementById("m").style.width = "10%";
+    document.getElementById("m").style.opacity = "1";
     document.getElementById("m").style.visibility= "visible";
    document.getElementById("zaslona").style.visibility= "visible";
    document.getElementById("zaslona").style.opacity= "0.5";
-   document.getElementById("container").style.overflow = "hidden"
+   document.getElementById("container").style.overflowX = "hidden"
    }
 }
 function hidemenu(){
     document.getElementById("m").style.width = "0%";
+    document.getElementById("m").style.opacity = "0";
     document.getElementById("m").style.visibility= "hidden";
     document.getElementById("zaslona").style.visibility= "hidden";
-    document.getElementById("root").style.opacity= "1";
-    document.getElementById("container").style.overflow = "visible";
+    document.getElementById("container").style.overflowY = "visible";
 }
 
 function refresh(){
@@ -50,13 +52,14 @@ window.onscroll = function() {scroll()};
 
  function scroll(){
 
-    if (document.documentElement.scrollTop > 50) {
+ 
+    if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 50) {
         document.getElementById("gora").style.backgroundColor = "white";
     
       } else {
         document.getElementById("gora").style.backgroundColor = "";
     
-      }
+      };
 
       if (document.documentElement.scrollTop > 450) {
         document.getElementById("divsearch1").style.width = "39%";
@@ -66,7 +69,26 @@ window.onscroll = function() {scroll()};
         document.getElementById("divsearch1").style.width = "0%";
       document.getElementById("divsearch1").style.visibility = "hidden";
       
-        }
+        };
 
+
+        
+      if (document.documentElement.scrollTop > 450 && window.innerWidth<1500) {
+        document.getElementById("divsearch1").style.width = "100%";
+        document.getElementById("divsearch1").style.height = "100%";
+        document.getElementById("divsearch1").style.left = "0%";
+         document.getElementById("divsearch1").style.visibility= "visible";
+         document.getElementById("guziki").style.width = "0%";
+         document.getElementById("guziki").style.visibility = "hidden";
+         document.getElementById("goraleft").style.width = "0%";
+         document.getElementById("goraleft").style.visibility = "hidden";
+      }
+      else {
+        document.getElementById("divsearch1").style.width = "0%";
+      document.getElementById("divsearch1").style.visibility = "hidden";
+      document.getElementById("guziki").style.width = "50%";
+         document.getElementById("guziki").style.visibility = "visible";
+         document.getElementById("goraleft").style.width = "40%";
+         document.getElementById("goraleft").style.visibility = "visible";
+        };
  }
- 
